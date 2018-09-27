976d0f52c36361019a461318295237917cb14043
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-09-27 02:21:41 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -37,9 +37,19 @@ class App < Sinatra::Base
     @operation = params[:operation]
     @number1 = params[:number1].to_i
     @number2 = params[:number2].to_i
-    
-    
-    
+
+    if @operation == "add"
+      (@number1 + @number2).to_s
+
+    elsif @operation == "subtract"
+      (@number1 - @number2).to_s
+
+    elsif @operation == "multiply"
+      (@number1 * @number2).to_s
+
+    elsif @operation == "divide"
+      (@number1 / @number2).to_s
+    end
   end
 
 end
